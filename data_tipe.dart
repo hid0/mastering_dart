e178036ad65410b42b6b2f0123579bbfe826bb48
Faiz Hidayatulloh <faiz14hiedayatoelloh@gmail.com>
2021-03-28 11:37:05 +0700

    data type

---
+++ data_tipe.dart
@@ -0,0 +1,9 @@
+main(List<String> args) {
+  var judul = "Belajar pemrograman dart";
+  int harga = 10000;
+  double berat = 2.31;
+
+  // cerak variabel
+  print(
+      'ini sedang $judul, lalu harga 1 pena yaitu $harga, sedangkan berat busa adalah $berat mg');
+}
